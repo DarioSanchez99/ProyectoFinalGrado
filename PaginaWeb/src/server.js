963c3335a7c9c8a3cf4a45817c51f641ecc34991
dario290799 <103026515+DarioSanchez99@users.noreply.github.com>
2022-06-01 16:27:00 +0200

    Actualizacion

--- PaginaWeb/src/server.js
+++ PaginaWeb/src/server.js
@@ -6,18 +6,18 @@ const session = require('express-session');
 const passport = require('passport');
 const morgan = require('morgan');
 
-// initializations
+
 const app = express();
 require('./database');
 require('./passport/local-auth');
 
-// settings
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'))
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
-// middlewares
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(session({
